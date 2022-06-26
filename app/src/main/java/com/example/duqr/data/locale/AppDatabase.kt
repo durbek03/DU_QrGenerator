09bf7a6b3b15c4d36f1fe31b01dc3ebef2bc8f56
durbek03 <ataboyevdurbek@gmail.com>
2022-06-26 14:28:53 +0500

    Room database deleted. Fetch qr bitmap implemented

--- app/src/main/java/com/example/duqr/data/locale/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.duqr.data.locale
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [QrCode::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun QrCodeDao() : QrCodeDao
-}
--- app/src/main/java/com/example/duqr/di/AppModule.kt
+++ app/src/main/java/com/example/duqr/di/AppModule.kt
@@ -1,15 +1,10 @@
 package com.example.duqr.di
 
-import android.content.Context
-import androidx.room.Room
 import com.example.duqr.constants.Constants
-import com.example.duqr.data.locale.AppDatabase
-import com.example.duqr.data.locale.QrCodeDao
 import com.example.duqr.data.remote.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -18,20 +13,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Singleton
-    @Provides
-    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
-        return Room.databaseBuilder(context, AppDatabase::class.java, "qrcodeDatabase")
-            .fallbackToDestructiveMigration().build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideQrCodeDao(appDatabase: AppDatabase): QrCodeDao {
-        return appDatabase.QrCodeDao()
-    }
-
     @Singleton
     @Provides
     fun provideApiService(): ApiService {
